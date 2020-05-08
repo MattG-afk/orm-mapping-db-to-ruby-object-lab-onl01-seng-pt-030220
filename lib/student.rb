f96bd3d92f2275eafe3a4393619a49853a43678b
Matt Gordon <bigmattsize21@gmail.com>
2020-05-08 17:40:03 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -57,12 +57,11 @@ class Student
     sql = <<-SQL
       SELECT * FROM students
       WHERE grade = 10
-      ORDER BY students.id
       LIMIT ?
     SQL
 
       DB[:conn].execute(sql, num).map do |row|
-        self.new_from_db(row[0])
+        self.new_from_db(row)
       end.first
   end
   
