6aa6378a8d5de9cd1a978d97273b574c522caf6a
Matt Gordon <bigmattsize21@gmail.com>
2020-05-08 16:17:33 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -51,7 +51,18 @@ class Student
   DB[:conn].execute(sql)
   end
   
-  
+  def self.first_student_in_grade_10
+    sql = <<-SQL
+      SELECT * FROM students
+      WHERE grade = 10 
+      ORDER BY students.id
+      LIMIT 1 
+    SQL
+    
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row[0])
+    end
+  end
   
   def save
     sql = <<-SQL
